b1f29800768cf925e88e952d0c8c0e7b7e471755
lunainfotec <kirtimadan1997@gmail.com>
2023-05-19 12:11:45 +0530

    added : UC10

--- employeePayRoll.js
+++ employeePayRoll.js
@@ -1,12 +1,16 @@
 class EmployeePayRoll{
     
-        get name() { return this.name; }
-       set name(name) { 
+        get name() 
+        { 
+            return this.name; 
+        }
+       
+        set name(name) { 
        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2, }$')
         if (nameRegex.test (name)) 
         this.name = name;
        
-       else throw 'Name 15 Incorrect';
+       else throw 'Name Incorrect';
        
        }    
 }
--- new_EmployeePayRoll.js
+++ new_EmployeePayRoll.js
@@ -6,5 +6,21 @@ salary.addEventListener('input', function()
      output.textContent = salary.value;
 });
 
+window.addEventListener('DOMContentLoaded', (event) => { 
+    const name = document.querySelector('#name'); 
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+     if (name.value.length == 0) {
+    textError.textContent = "";
+    return;
+    }
+    try {
+    (new EmployeePayRoll()).name = name.value;
+    textError.textContent = " ";
+    } catch (e) {
+    textError.textContent = e;
+    }
+    });
+    });
 
 
